b6b0eaa6dcf00a30374b353bb88c2c9341dcbb74
Евгений Нестеров <e.nesterov@inventos.ru>
2016-12-19 19:22:53 +0300

    Init commit

---
+++ main.c
@@ -0,0 +1,33 @@
+/*
+
+We wish you a merry Christmas,
+We wish you a merry Christmas,
+We wish you a merry Christmas,
+And a happy New Year!
+
+Glad tidings we bring
+To you and your kin;
+Glad tidings for Christmas
+And a happy New Year!
+
+We want some figgy pudding,
+We want some figgy pudding,
+We want some figgy pudding,
+Please bring it right here!
+
+For we all like figgy pudding,
+for we all like figgy pudding,
+For we all like figgy pudding:
+so bring some out here!
+
+We won't go until we get some,
+We won't go until we get some,
+We won't go until we get some,
+So bring it out here!
+
+We wish you a Merry Christmas,
+We wish you a Merry Christmas,
+We wish you a Merry Christmas
+And a happy New Year.
+
+*/
